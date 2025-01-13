2213dfca1730ae3068482fb8d4108c9b210a6a69
hemakodaa <hemakodaa@gmail.com>
2025-01-13 21:09:04 +0700

    speed up message processing

--- src/chat_activity/main.py
+++ src/chat_activity/main.py
@@ -1,7 +1,7 @@
 from chat_downloader import ChatDownloader
 from chat_downloader.sites.common import Chat
 import matplotlib.pyplot as plt
-import datetime
+from datetime import datetime
 from matplotlib.pyplot import figure
 import matplotlib.ticker as ticker
 import requests
@@ -23,34 +23,26 @@ def get_title() -> str:
 
 def chat() -> Chat:
     chat_download_start = time.time()
-    chat: Chat = ChatDownloader().get_chat(URL)
+    c: Chat = ChatDownloader().get_chat(URL)
     print(f"Total chat download runtime: {time.time() - chat_download_start}")
-    return chat
+    return c
+
+
+def calculate_chat_live_timestamp(message: int, stream_start: datetime):
+    duration = stream_start - datetime.fromtimestamp(message / 1_000_000)
+    duration_in_minutes = abs(divmod(duration.total_seconds(), 60)[0])
+    return duration_in_minutes
 
 
 def message_processing():
-    stream_start = 0
-    started = False
     time_list = []
     message_processing_start = time.time()
-    for message in chat():
-        message_seconds = message.get("time_in_seconds")
-        # skips all messages that are sent before stream starts
-        if message_seconds < 0:
-            continue
-        # started => the first time the time_in_seconds is bigger than zero
-        # means it's the stream start time
-        # this must run ONLY ONCE
-        if not started:
-            stream_start = datetime.datetime.fromtimestamp(
-                message.get("timestamp") / 1_000_000
-            )  # timestamp is in unix epoch time (microseconds)
-            started = True
-
-        message_datetime = message.get("timestamp") / 1_000_000
-        duration = stream_start - datetime.datetime.fromtimestamp(message_datetime)
-        duration_in_minutes = abs(divmod(duration.total_seconds(), 60)[0])
-        time_list.append(duration_in_minutes)
+    all_live_chat = [c for c in chat() if c.get("time_in_seconds") > 0]
+    stream_start = datetime.fromtimestamp(all_live_chat[0].get("timestamp") / 1_000_000)
+    for c in all_live_chat:
+        time_list.append(
+            calculate_chat_live_timestamp(c.get("timestamp"), stream_start)
+        )
     print(f"Total message processing runtime: {time.time() - message_processing_start}")
     return time_list
 
